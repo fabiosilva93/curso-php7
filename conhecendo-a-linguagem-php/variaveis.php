5a77f275aba9c7b77d762b225ed7deb3bae75f31
Fabio Silva <fabiosilvamjr@gmail.com>
2019-02-15 20:52:05 -0200

    git inicial curso php7

---
+++ conhecendo-a-linguagem-php/variaveis.php
@@ -0,0 +1,10 @@
+<?php
+
+$nome = "Fabio";
+$sobrenome = "Silva";
+$nomecompleto = $nome . " " . $sobrenome;
+
+//var_dump é usado para mostrar o tipo da variável, o tamanho e o valor contido nela.
+var_dump($nomecompleto);
+
+?>
